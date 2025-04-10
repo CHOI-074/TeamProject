f740cb956ff637f4ca508eeb624e9e49e756b235
hyebinJeong <138184978+hyebinJeong@users.noreply.github.com>
2025-04-10 10:40:56 +0900

    Update index.js
    - index.js 병합

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,22 @@
+import CreateWrapperPage from '@/views/CreateWrapperPage.vue';
+import EditWrapperPage from '@/views/EditWrapperPage.vue';
 import { createRouter, createWebHistory } from 'vue-router';
-import Filter from '@/views/Filter.vue';
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Filter,
-  },
-];
-
+// 라우터 인스턴스 생성
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/record/create',
+      name: 'createWrapperPage',
+      component: CreateWrapperPage,
+    },
+    {
+      path: '/record/edit/:id',
+      name: 'editWrapperPage',
+      component: EditWrapperPage,
+    },
+  ],
 });
 
 export default router;
