cdcb6558074cc0c18f678ea8ff2d0f7ec885e3bd
choichangyun <tuffhrgha704@naver.com>
2025-04-10 13:53:41 +0900

    main.js수정

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './assets/index.css';
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue';
 import router from './router';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import './assets/index.css';
 
 const app = createApp(App);
 const pinia = createPinia();
