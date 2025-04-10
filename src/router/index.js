c04bbcfa1c1f8e3c1aa596a3371a24dce06937f5
lee030327 <lee030327@naver.com>
2025-04-10 11:32:53 +0900

    모바일 규격으로 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '../views/Main.vue'
+import NotFoundPage from '../views/NotFoundPage.vue'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'MainPage',
+    component: MainPage,
+  },
+  {
+    path: '/not-found',
+    name: 'NotFound',
+    component: NotFoundPage,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFoundPage,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
