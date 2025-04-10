6e21e1bffdcce0634e4d8cab5704ae51c7af3bd5
choichangyun <tuffhrgha704@naver.com>
2025-04-10 13:50:24 +0900

    패키지 수정

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+import './assets/index.css';
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue';
 import router from './router';
-import { createPinia } from 'pinia';
-import './index.css';
 
 const app = createApp(App);
-app.use(createPinia());
+const pinia = createPinia();
+
+// 플러그인 등록
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
 app.use(router);
 app.mount('#app');
