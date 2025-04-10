e5e26599a87aaeeee956859c855c2d972b935be9
LSY0108 <seungye1209@gmail.com>
2025-04-10 13:42:56 +0900

    login update

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
+<script setup></script>
+
 <template>
   <div id="app">
-    <!-- <h1 class="text-2xl font-bold text-center mb-4">필터링 기능</h1> -->
     <router-view />
   </div>
 </template>
-
-<script>
-
-export default {
-  name: 'App',
-  components: {
-  },
-};
-</script>
-
-<style>
-body {
-  font-family: 'Arial', sans-serif;
-}
-</style>
